c25e57ff266b98439dbcebd51645a6ab354b2d16
huytran <trandoanhuy.work@gmail.com>
2023-06-08 10:08:24 +0700

    Connect mongodb with lv0

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
+require("dotenv").config();
 
 const app = express();
 
@@ -11,6 +12,7 @@ app.use(helmet());
 app.use(compression());
 
 // init db
+require("./dbs/init.mongodb.lv0");
 
 // init router
 app.get("/", (req, res, next) => {
